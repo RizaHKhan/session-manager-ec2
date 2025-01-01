65e28f3aa9f750b9046cc03bdac8d22f53d9d400
khanr <khanriza@gmail.com>
2024-12-31 20:50:03 -0500

    Initial commit

---
+++ test/session-manager-ec2.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SessionManagerEc2 from '../lib/session-manager-ec2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/session-manager-ec2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SessionManagerEc2.SessionManagerEc2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
